fec6bef29c34badfa57e4c9e05b41dcc52f1bfa9
Dmytro Fedorovych <ipz213_fdm@student.ztu.edu.ua>
2023-03-23 19:10:44 +0200

    first commit

---
+++ WebApplication1/Controllers/HelloControllerPasha.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HelloControllerPasha : Controller
+    {
+        [HttpGet, Route("Home/Index"), Route("")]
+        public ActionResult Index(Name Con)
+        {
+            return View("~/Views/Pasha/Index.cshtml", Con);
+        }
+
+        [HttpPost]
+        public IActionResult Welcome(Name ConName)
+        {
+            return View("~/Views/Pasha/Welcome.cshtml", ConName);
+        }
+    }
+}
---
+++ WebApplication1/Models/Name.cs
@@ -0,0 +1,7 @@
+﻿namespace WebApplication1.Models
+{
+    public class Name
+    {
+        public string FirstName { get; set; }
+    }
+}
